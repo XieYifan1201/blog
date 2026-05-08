e63e3646fd465d8cb003c97af56acceacb2ee3b1
XieYifan <xieyifan@MacBook-Air.local>
2026-05-08 11:02:28 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://20051201.xyz',
+    site: 'https://www.20051201.xyz',
     devToolbar: {
         enabled: false,
     },
